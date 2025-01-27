42dc70986517814f8f7a84be516a49ba3cd94d4f
Babacar NIANG <babacar.niang@intouchgroup.net>
2025-01-27 12:40:35 +0000

    correction on res partner

--- .history/partner_readonly_import/models/res_partner_custum_20250127122935.py
+++ .history/partner_readonly_import/models/res_partner_custum_20250127122935.py
@@ -7,16 +7,17 @@ class ContactDataCorrection(models.Model):
 
     @api.model
     def correct_contact_data(self):
-        """
-        Cette méthode corrige les contacts où le prénom et le nom sont inversés.
-        """
         contacts = self.search([('is_company', '=', False), ('x_correction_done', '=', False)])
-
         for contact in contacts:
             if contact.first_name and contact.last_name:
-                # Inverser les champs prénom et nom
                 contact.write({
                     'first_name': contact.last_name,
                     'last_name': contact.first_name,
                     'x_correction_done': True,
                 })
+
+    @api.model
+    def set_contacts_read_only(self):
+        contacts = self.search([('is_company', '=', False), ('x_correction_done', '=', True)])
+        for contact in contacts:
+            contact.write({'read_only': True})
