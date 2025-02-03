7010ff5c8adbb9340244292b195ffa683c280ef4
Babacar NIANG <babacar.niang@intouchgroup.net>
2025-02-03 17:53:16 +0000

    Module 3cx to add call log

--- 3cx_extension/__manifest__.py
+++ 3cx_extension/__manifest__.py
@@ -2,7 +2,7 @@
     'name': '3CX Call Log Extension',
     'version': '1.0',
     'summary': 'Extend nalios_3cx without modifying the base module',
-    'depends': ['nalios_3cx'],
+    'depends': ['nalios_3cx_full'],
     'data': [
         'views/res_partner_views.xml',
     ],
